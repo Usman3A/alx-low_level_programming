771175fa449ebbe6842aa41f4b395b3586a124a4
Usman3A <usmansirajo72@gmail.com>
2024-02-29 23:21:19 -0800

    j

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
+* main - causes an inffinite loop
 * Return: 0
 */
 
@@ -19,5 +19,6 @@ int main(void)
 	*}
 	*/
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
