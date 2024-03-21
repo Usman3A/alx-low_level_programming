d7152bee06f0d9af39d914fbf720f185a298c092
Usman3A <usmansirajo72@gmail.com>
2024-03-21 13:54:49 -0700

    g

--- 0x17-doubly_linked_lists/palindrome.py
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-def ispalindrome(n):
-    string = str(n)
-    return string == string[::-1]
 
-array = []
-for i in range(999, 99, -1):
-    for j in range(i, 99, -1):
-        if ispalindrome(i * j):
-            array.append(i * j)
-print(max(array))
+
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+largest_palindrome = 0
+
+for i in range(100, 1000):
+    for j in range(100, 1000):
+        product = i * j
+        if is_palindrome(product) and product > largest_palindrome:
+            largest_palindrome = product
+
+with open('102-result', 'w') as f:
+    f.write(str(largest_palindrome))
